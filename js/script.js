63295b42d870da6470e30c0dda42a4404d4124a4
Margo0107 <megi.busenka@gmail.com>
2025-05-18 23:50:31 +0300

    task 12.2 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//12.1
 let savedLink = "";
 document.getElementById("setLink").addEventListener("click", function () {
   savedLink = prompt("Enter the link:");
@@ -7,5 +8,14 @@ document.getElementById("goToLink").addEventListener("click", function () {
     window.location.href = savedLink;
   } else {
     alert("Enter the beginning!");
+  }
+});
+
+//12.2
+const container = document.getElementById("buttonContainer");
+
+container.addEventListener("click", function (event) {
+  if (event.target.tagName === "BUTTON") {
+    alert("You pressed on: " + event.target.textContent);
   }
 });
