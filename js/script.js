2645ee4b3dcddbc3fbca7584c53b1dbbee0247b6
Margo0107 <megi.busenka@gmail.com>
2025-05-19 20:44:42 +0300

    task 12.3 completed

--- js/script.js
+++ js/script.js
@@ -19,3 +19,26 @@ container.addEventListener("click", function (event) {
     alert("You pressed on: " + event.target.textContent);
   }
 });
+
+//12.3
+const taskList = document.getElementById("taskList");
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+
+taskList.addEventListener("click", function (event) {
+  if (event.target.classList.contains("deleteBtn")) {
+    event.target.parentElement.remove();
+  }
+});
+addBtn.addEventListener("click", function () {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const newItem = document.createElement("li");
+    newItem.classList.add("lis-text");
+    newItem.innerHTML = taskText + '<button class="deleteBtn">Delete</button>';
+    taskList.appendChild(newItem);
+    taskInput.value = "";
+  } else {
+    alert("Enter text!");
+  }
+});
